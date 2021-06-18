5f0684e8e2afbfd708eda9c14a0810f2daee7e34
Lucas Angelo <angelucas@gmail.com>
2021-06-18 12:01:28 -0300

    Updates

--- assets/code/Startup.cs
+++ assets/code/Startup.cs
@@ -1,16 +1,15 @@
-	// This method gets called by the runtime. Use this method to add services to the container.
-	public void ConfigureServices(IServiceCollection services)
-	{
-		services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-			.AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+public void ConfigureServices(IServiceCollection services)
+{
+	services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+		.AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
 
-		services.AddControllersWithViews(options =>
-		{
-			var policy = new AuthorizationPolicyBuilder()
-				.RequireAuthenticatedUser()
-				.Build();
-			options.Filters.Add(new AuthorizeFilter(policy));
-		});
-	   services.AddRazorPages()
-			.AddMicrosoftIdentityUI();
-	}
+	services.AddControllersWithViews(options =>
+	{
+		var policy = new AuthorizationPolicyBuilder()
+			.RequireAuthenticatedUser()
+			.Build();
+		options.Filters.Add(new AuthorizeFilter(policy));
+	});
+	services.AddRazorPages()
+		.AddMicrosoftIdentityUI();
+}
